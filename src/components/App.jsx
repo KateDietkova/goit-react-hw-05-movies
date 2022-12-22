5f67c7f038d07a54f6529a5fd06bc76acc7b420f
Katerina Dietkova <katrinkir1296@gmail.com>
2022-12-22 16:40:16 +0200

    add Routes to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="/movies/:movieId" element={<div>MovieDetails</div>}>
+          <Route path="cast" element={<div>Cast</div>} />
+          <Route path="reviews" element={<div>Reviews</div>} />
+        </Route>
+      </Routes>
     </div>
   );
 };
